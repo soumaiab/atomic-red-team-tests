467507af27814f1866896df6019bf87774291aed
soumaiab <soumaia.bouhouia@mail.concordia.ca>
2026-08-01 19:24:08 -0400

    doc: Added logs for credential access

--- Data/search_after_export.ps1
+++ Data/search_after_export.ps1
@@ -1,7 +1,7 @@
 # === CONFIGURATION ===
-$startTime = "2026-08-01T22:57:00.000Z"
-$endTime   = "2026-08-01T23:03:00.000Z"
-$agentName = "windows-15"
+$startTime = "2026-08-01T19:54:00.000Z"
+$endTime   = "2026-08-01T23:12:00.000Z"
+$agentName = "windows-12"
 $size = 10000
 $index = 1
 $lastSort = $null
