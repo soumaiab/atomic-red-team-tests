fa09f08dad7f6c51b3a5d304d366b1947b7b8e6e
soumaiab <soumaia.bouhouia@mail.concordia.ca>
2026-08-01 21:05:35 -0400

    doc: added stealth logs run2.2

--- Data/search_after_export.ps1
+++ Data/search_after_export.ps1
@@ -1,7 +1,7 @@
 # === CONFIGURATION ===
 $startTime = "2026-08-01T23:45:00.000Z"
-$endTime   = "2026-08-02T00:20:00.000Z"
-$agentName = "windows-11"
+$endTime   = "2026-08-02T00:25:00.000Z"
+$agentName = "windows-12"
 $size = 10000
 $index = 1
 $lastSort = $null
